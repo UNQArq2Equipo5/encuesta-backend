29c64938159fa785fc1b10a889bc6b5302984823
hslavich <hernan.slavich@gmail.com>
2016-11-04 17:03:46 -0300

    Datos demo

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,7 +11,17 @@
 
 module.exports.bootstrap = function(cb) {
 
+  Materia.create({
+    nombre: 'Orga',
+    opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+  }).exec(function (err){
+    if (err) { return done(err); }
+
+    // ...etc...
+    return cb();
+  });
+
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  // cb();
 };
