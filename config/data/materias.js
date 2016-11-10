8028f7753dc18a1d22baa08c040a61c7b9ed6c3f
hslavich <hernan.slavich@gmail.com>
2016-11-10 16:29:19 -0300

    Materias precargadas

---
+++ config/data/materias.js
@@ -0,0 +1,124 @@
+module.exports = {
+  data: [
+    {
+      nombre: 'Introducción a la Programación',
+      nucleo: 'núcleo básico obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Organización de Computadoras',
+      nucleo: 'núcleo básico obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Matemática',
+      nucleo: 'núcleo básico obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Programación con Objetos I',
+      nucleo: 'núcleo básico obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Bases de Datos',
+      nucleo: 'núcleo básico obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Estructuras de Datos',
+      nucleo: 'núcleo básico obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Programación con Objetos II',
+      nucleo: 'núcleo básico obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Redes de Computadoras',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Sistemas Operativos',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Programación Concurrente',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Matemática II',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Elementos de Ingeniería de Software',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Construcción de Interfaces de Usuario',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Estrategias de Persistencia',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Programación Funcional',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Desarrollo de Aplicaciones',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Laboratorio de Sistemas Operativos y Redes',
+      nucleo: 'núcleo avanzado obligatorio',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Seguridad Informática',
+      nucleo: 'núcleo complementario',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Bases de Datos II',
+      nucleo: 'núcleo complementario',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Participación y Gestión en Proyectos de Software Libre',
+      nucleo: 'núcleo complementario',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Introducción a las Arquitecturas de Software',
+      nucleo: 'núcleo complementario',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Programación con Objetos III',
+      nucleo: 'núcleo complementario',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Introducción al Desarrollo de Videojuegos',
+      nucleo: 'núcleo complementario',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    },
+    {
+      nombre: 'Seminarios',
+      nucleo: 'núcleo complementario',
+      opciones: ['Comisión 1', 'Comisión 2', 'Comisión 3']
+    }
+  ]
+}
