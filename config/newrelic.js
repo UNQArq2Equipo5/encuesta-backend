abfa605b7e3f2bf6a1904b0dc3d3c732daf0bca6
Gustavo Pilla <pilla.gustavo@gmail.com>
2016-12-04 01:42:49 -0300

    Fix newrelic

--- config/newrelic.js
+++ config/newrelic.js
@@ -2,7 +2,7 @@ module.exports.newrelic = {
   app_name: ['encuesta-backend'],
   license_key: '1d6c0f4135bbbf38a3affa34b5e14b9b91802c47',
   logging: {
-    level: 'info', // can be error, warn, info, debug or trace
+    level: 'debug', // can be error, warn, info, debug or trace
   },
   rules: {
       ignore: ['^/socket.io/.*/xhr-polling']
