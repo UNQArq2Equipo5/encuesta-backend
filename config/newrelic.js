500b11950b0733060dfce5094b578f44b3a21ad1
Gustavo Pilla <pilla.gustavo@gmail.com>
2016-12-04 01:38:16 -0300

    Fix newrelic

--- config/newrelic.js
+++ config/newrelic.js
@@ -1,5 +1,5 @@
 module.exports.newrelic = {
-  app_name: ['Clink AR'],
+  app_name: ['encuesta-backend'],
   license_key: '1d6c0f4135bbbf38a3affa34b5e14b9b91802c47',
   logging: {
     level: 'info', // can be error, warn, info, debug or trace
