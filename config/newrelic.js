b48aca6390168ad8f587fadc3463f01738d939f7
Gustavo Pilla <pilla.gustavo@gmail.com>
2016-12-04 01:21:10 -0300

    Se agrega newrelic

---
+++ config/newrelic.js
@@ -0,0 +1,10 @@
+module.exports.newrelic = {
+  app_name: ['Clink AR'],
+  license_key: '1d6c0f4135bbbf38a3affa34b5e14b9b91802c47',
+  logging: {
+    level: 'info', // can be error, warn, info, debug or trace
+  },
+  rules: {
+      ignore: ['^/socket.io/.*/xhr-polling']
+  }
+};
