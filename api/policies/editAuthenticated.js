c5d06151271134eeba63f31223d426d1a47f806a
hslavich <hernan.slavich@gmail.com>
2017-04-12 18:49:24 -0300

    Cambio en policy para permitir autenticacion http

--- api/policies/editAuthenticated.js
+++ api/policies/editAuthenticated.js
@@ -1,10 +1,20 @@
+var passport = require('passport');
+
 /**
  * Requiere autenticacion metodo != GET (POST, DELETE, etc)
  */
 module.exports = function(req, res, next) {
-    if (req.method != 'GET' && !req.isAuthenticated()) {
-        return res.forbidden('You are not permitted to perform this action.');
-    }
+    if (req.method != 'GET') {
+        if (req.isAuthenticated()) {
+            return next();
+        }
 
-    return next();
+        passport.authenticate('basic', {session: false}, function(err, user) {
+            if (!user) return res.forbidden('You are not permitted to perform this action.');
+            req.user = user;
+            return next();
+        })(req, res, next);
+    } else {
+        return next();
+    }
 };
