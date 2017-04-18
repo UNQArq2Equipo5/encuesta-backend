2da3dd9431e04b1fd4285420df7cfbd0ccf88631
hslavich <hernan.slavich@gmail.com>
2017-04-18 13:20:47 -0300

    Refactor

--- config/passport.js
+++ config/passport.js
@@ -13,42 +13,23 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
-passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'password'},
-    function(email, password, done) {
-        User.findOne({ email: email }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) { return done(null, false, { message: 'Incorrect email.' }); }
+authenticateUser = function(email, password, done) {
+    User.findOne({ email: email }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Incorrect email.' }); }
 
-            bcrypt.compare(password, user.password, function (err, res) {
-                if (!res) return done(null, false, { message: 'Invalid Password'});
-                var returnUser = {
-                    email: user.email,
-                    createdAt: user.createdAt,
-                    id: user.id
-                };
+        bcrypt.compare(password, user.password, function (err, res) {
+            if (!res) return done(null, false, { message: 'Invalid Password'});
+            var returnUser = {
+                email: user.email,
+                createdAt: user.createdAt,
+                id: user.id
+            };
 
-                return done(null, returnUser, { message: 'Logged In Successfully' });
-            });
+            return done(null, returnUser, { message: 'Logged In Successfully' });
         });
-    }
-));
+    });
+}
 
-passport.use(new BasicStrategy(
-    function(email, password, done) {
-        User.findOne({ email: email }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) { return done(null, false, { message: 'Incorrect email.' }); }
-
-            bcrypt.compare(password, user.password, function (err, res) {
-                if (!res) return done(null, false, { message: 'Invalid Password'});
-                var returnUser = {
-                    email: user.email,
-                    createdAt: user.createdAt,
-                    id: user.id
-                };
-
-                return done(null, returnUser, { message: 'Logged In Successfully' });
-            });
-        });
-    }
-));
+passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'password'}, authenticateUser));
+passport.use(new BasicStrategy(authenticateUser));
