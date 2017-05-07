0ae793a5011fd364b06166b5b2a4c8938a8657f9
David Leonhardt <davidleonhardt@gmail.com>
2017-05-07 03:54:26 -0300

    update newrelic key

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,6 @@
 exports.config = {
   app_name: ['encuesta-arq2'],
-  license_key: '098569dcab84ccb73cfb478c2f30932094d2c90c',
+  license_key: 'f8ae3c30a9e995f16ce779adb86f7793a7d39bd3',
   logging: {
     level: 'debug' // can be error, warn, info, debug or trace
   },
